dd6393e947a009fc455c2ab0b5fe34840f73e3ec
Grygory Ksheminsky <maroon785@gmail.com>
2022-05-21 02:40:55 +0300

    working with onChange and onSubmit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
+  const [searchWord, setSearchWord] = useState('');
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    //make sure calling callback from parent
+  };
+
   return (
     <div className="search-bar ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={onFormSubmit}>
         <div className="field">
           <label>Search Video</label>
-          <input type="text" />
+          <input type="text" value={searchWord} onChange={(e) => setSearchWord(e.target.value)} />
         </div>
       </form>
     </div>
