aaca24421f9bad4e92eb90c09bd40653350d5ade
Grygory Ksheminsky <maroon785@gmail.com>
2022-05-21 01:50:33 +0300

    setting up an architecture and adding basic jsx

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Search from './Search';
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <Search />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div className="search-bar ui segment">
+      <form className="ui form">
+        <div className="field">
+          <label>Search Video</label>
+          <input type="text" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
