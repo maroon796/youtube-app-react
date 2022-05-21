2184b8d0ab132b2c0dc008e8ad71a230605a9b80
Grygory Ksheminsky <maroon785@gmail.com>
2022-05-21 19:07:30 +0300

    styling VideoItem component

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
   return (
-    <div>
+    <div className="ui relaxed divided list">
       {videos.map((video) => {
         return <VideoItem video={video} />;
       })}
