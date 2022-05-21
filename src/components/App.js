ee8379fc98f9346a92d0d749920dea0b5fa9a668
Grygory Ksheminsky <maroon785@gmail.com>
2022-05-21 18:35:27 +0300

    working with api and setting another components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Search from './Search';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 const App = () => {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const onWordSubmit = (word) => {
-    console.log(word);
+
+  useEffect(() => {
+    onWordSubmit('witcher 3');
+  }, []);
+
+  const onWordSubmit = async (word) => {
+    const res = await youtube.get('/search', {
+      params: {
+        q: word,
+      },
+    });
+    setVideos(res.data.items);
+    setSelectedVideo(res.data.items[0]);
   };
+
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
+  };
+
   return (
     <div className="ui container">
       <Search onFormSubmit={onWordSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
